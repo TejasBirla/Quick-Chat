35546e87640079d26a438d073a7254dd8a08bff3
Tejas Birla <tejasbirla3@gmail.com>
2025-11-06 12:20:14 +0530

    function updated

--- server/lib/resendApiMail.js
+++ server/lib/resendApiMail.js
@@ -2,7 +2,7 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendEmail = async (to, subject, html) => {
+const sendEmail = async (to, subject, html) => {
   try {
     await resend.emails.send({
       from: process.env.EMAIL_USER,
@@ -16,3 +16,4 @@ export const sendEmail = async (to, subject, html) => {
   }
 };
 
+export default sendEmail;
